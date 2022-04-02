9f627ea40ddd9dbec9ce54d2cb7ea6c14cd9858c
KlaussMarchi <marchiklauss@hotmail.com>
2022-04-02 15:46:39 -0300

    atualizacao 9.0

---
+++ ESP32CAM/ESP32CAM/UPLOAD PYTHON.py
@@ -0,0 +1,31 @@
+import cv2
+import urllib.request
+import numpy as np
+
+url = 'http://192.168.68.110/cam-hi.jpg'
+
+def run1():
+    # CRIANDO UMA JANELA PADRÃO MESMO SEM RECEBER A IMAGEM
+    cv2.namedWindow("WEB IMAGE", cv2.WINDOW_AUTOSIZE)
+
+    while True:
+        # RECEBENDO AS INFORMAÇÕES CONTIDAS NO ENDEREÇO INDICADO
+        WEBinfo = urllib.request.urlopen(url)
+
+        # CONVERTENDO A INFORMAÇÃO PARA UM ARRAY DE BYTES TIPO UINT8
+        img = np.array(bytearray(WEBinfo.read()), dtype=np.uint8)
+        img = cv2.imdecode(img, -1)
+
+        # MOSTRANDO A IMAGEM
+        cv2.imshow('WEB IMAGE', img)
+        key = cv2.waitKey(5)
+
+        # SAINDO SE A TECLA 'q' FOR PRESSIONADA
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+
+
+run1()
