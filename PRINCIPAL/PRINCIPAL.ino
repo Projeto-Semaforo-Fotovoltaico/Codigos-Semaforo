92b95153b37585f26b5405926197a7a4574964be
KlaussMarchi <marchiklauss@hotmail.com>
2022-05-03 23:28:29 -0300

    atualizacao 10.1

--- PRINCIPAL/PRINCIPAL.ino
+++ PRINCIPAL/PRINCIPAL.ino
@@ -3,12 +3,18 @@
 
 WiFiServer server(80);
 #define LED 14
+#define MAX 10
+
+bool vetor[MAX];
+int i = 0;
+
 
 // LIMPANDO MEMÓRIA FLASH
 void tcpCleanup(){
     while (tcp_tw_pcbs != NULL)
         tcp_abort(tcp_tw_pcbs);
 }
+
 
 // CRIANDO E INICIANDO O ROTEADOR LOCAL
 void startServer(char* nome, char* senha){
@@ -28,6 +34,48 @@ void startServer(char* nome, char* senha){
 }
 
 
+// CALCULANDO A MÉDIA DE UM VETOR DE TAMANHO MAX
+float mediaVetor(bool vetor[]){
+    float soma = 0;
+    
+    for(byte x=0; x<MAX; x++)
+        soma += (float) vetor[x];
+  
+    return soma/MAX;
+}
+
+
+// ZERANDO TODAS AS COMPONENTES DE UM VETOR DE TAMANHO MAX
+void zeraVetor(bool *vetor){
+    for(byte x=0; x<MAX; x++)
+        vetor[x] = 0;
+}
+
+
+// PROCESSANDO A REQUISIÇÃO RECEBIDA PARA ACENDER OS LEDS
+void processaRequisicao(String requisicao){
+    Serial.print("REQUISICAO: "); Serial.println(requisicao); 
+    
+    if(requisicao.indexOf("ATIVAR") != -1)
+        vetor[i] = 1;
+  
+    if(requisicao.indexOf("DESATIVAR") != -1)
+        vetor[i] = 0;
+  
+    if(i == MAX - 1){
+        float media = mediaVetor(vetor);
+        
+        if(media > 0.5)
+            digitalWrite(LED, HIGH);
+        else
+            digitalWrite(LED, LOW);
+        
+        i = 0;
+        zeraVetor(vetor);
+    }
+}
+
+
 // ESTABELECENDO A COMUNICAÇÃO WIFI E MONITOR SERIAL
 void setup() {
     Serial.begin(9600); 
@@ -39,6 +87,8 @@ void setup() {
     digitalWrite(LED, HIGH);
     delay(2000);
     digitalWrite(LED, LOW);
+
+    zeraVetor(vetor);
 }
 
 
@@ -55,16 +105,10 @@ void loop() {
     while (!client.available())
       delay(1);
     
-    // LENDO A REQUISIÇÃO RECEBIDA
+    // LENDO A REQUISIÇÃO RECEBIDA E AUMENTANDO i
     String requisicao = client.readStringUntil('\r');
-    Serial.print("REQUISICAO: ");
-    Serial.println(requisicao); 
-    
-    if(requisicao.indexOf("ATIVAR") != -1)
-      digitalWrite(LED, HIGH);
-
-    if(requisicao.indexOf("DESATIVAR") != -1)
-      digitalWrite(LED, LOW);
+    processaRequisicao(requisicao);
+    i++;
     
     client.flush();
     client.stop();
